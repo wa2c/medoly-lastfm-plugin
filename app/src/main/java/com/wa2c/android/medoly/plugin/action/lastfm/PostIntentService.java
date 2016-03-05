01e0167c01350188caae63a1de16498af1fe6239
entry@wa2c.com <entry@wa2c.com>
2016-03-06 04:51:24 +0900

    ・トラックページ、アーティストページ表示機能追加

--- app/src/main/java/com/wa2c/android/medoly/plugin/action/lastfm/PostIntentService.java
+++ app/src/main/java/com/wa2c/android/medoly/plugin/action/lastfm/PostIntentService.java
@@ -22,6 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 
+import de.umass.lastfm.Artist;
 import de.umass.lastfm.Authenticator;
 import de.umass.lastfm.Caller;
 import de.umass.lastfm.Result;
@@ -51,6 +52,10 @@ public class PostIntentService extends IntentService {
         LOVE,
         /** Unlove */
         UNLOVE,
+        /** Page Track */
+        PAGE_TRACK,
+        /** Page Artist */
+        PAGE_ARTIST,
         /** Ban */
         BAN,
         /** Unban */
@@ -145,12 +150,12 @@ public class PostIntentService extends IntentService {
             if (categories.contains(PluginOperationCategory.OPERATION_PLAY_START.getCategoryValue())) {
                 // Play Start
                 if (!isEvent || sharedPreferences.getBoolean(context.getString(R.string.prefkey_operation_play_start_enabled), false)) {
-                    startPost(PostType.SCROBBLE);
+                    post(PostType.SCROBBLE);
                 }
             } else if (categories.contains(PluginOperationCategory.OPERATION_PLAY_NOW.getCategoryValue())) {
                 // Play Now
                 if (!isEvent || sharedPreferences.getBoolean(context.getString(R.string.prefkey_operation_play_now_enabled), true)) {
-                    startPost(PostType.SCROBBLE);
+                    post(PostType.SCROBBLE);
                 }
             } else if (categories.contains(PluginOperationCategory.OPERATION_EXECUTE.getCategoryValue())) {
                 // Execute
@@ -158,21 +163,29 @@ public class PostIntentService extends IntentService {
                 final String EXECUTE_UNLOVE_ID = "execute_id_unlove";
                 final String EXECUTE_BAN_ID = "execute_id_ban";
                 final String EXECUTE_UNBAN_ID = "execute_id_unban";
+                final String EXECUTE_PAGE_TRACK = "execute_id_page_track";
+                final String EXECUTE_PAGE_ARTIST = "execute_id_page_artist";
                 final String EXECUTE_SITE_ID = "execute_id_site";
 
                 if (extras != null) {
                     if (extras.keySet().contains(EXECUTE_LOVE_ID)) {
                         // Love
-                        startPost(PostType.LOVE);
+                        post(PostType.LOVE);
                     } else if (extras.keySet().contains(EXECUTE_UNLOVE_ID)) {
                         // UnLove
-                        startPost(PostType.UNLOVE);
+                        post(PostType.UNLOVE);
                     } else if (extras.keySet().contains(EXECUTE_BAN_ID)) {
                         // Ban
-                        startPost(PostType.BAN);
+                        post(PostType.BAN);
                     } else if (extras.keySet().contains(EXECUTE_UNBAN_ID)) {
                         // UnBan
-                        startPost(PostType.UNBAN);
+                        post(PostType.UNBAN);
+                    } else if (extras.keySet().contains(EXECUTE_PAGE_TRACK)) {
+                        // Page Track
+                        post(PostType.PAGE_TRACK);
+                    } else if (extras.keySet().contains(EXECUTE_PAGE_ARTIST)) {
+                        // Page Artist
+                        post(PostType.PAGE_ARTIST);
                     } else if (extras.keySet().contains(EXECUTE_SITE_ID)) {
                         // Last.fm
                         String username = sharedPreferences.getString(context.getString(R.string.prefkey_auth_username), "");
@@ -205,50 +218,47 @@ public class PostIntentService extends IntentService {
 
 
     /**
-     * 投稿。
-     * @param postType 投稿k種別。
+     * 投稿を開始。
+     * @param postType 投稿種別。
      */
-    @SuppressWarnings("unchecked")
-    private void startPost(PostType postType) {
+    private void post(PostType postType) {
         // 音楽データ無し
         if (mediaUri == null) {
             AppUtils.showToast(getApplicationContext(), R.string.message_no_media);
+            showResult(PostResult.IGNORE, postType);
             return;
         }
 
         // 必須情報無し
         if (TextUtils.isEmpty(propertyMap.get(MediaProperty.TITLE.getKeyName())) ||
             TextUtils.isEmpty(propertyMap.get(MediaProperty.ARTIST.getKeyName()))) {
+            showResult(PostResult.IGNORE, postType);
             return;
         }
 
+        // 認証確認
+        String username = sharedPreferences.getString(context.getString(R.string.prefkey_auth_username), "");
+        String password = sharedPreferences.getString(context.getString(R.string.prefkey_auth_password), "");
+        if (TextUtils.isEmpty(username)) {
+            showResult(PostResult.AUTH_FAILED, postType);
+            return;
+        }
+
+        // 前回メディア確認
         if (postType == PostType.SCROBBLE) {
             String mediaUriText = mediaUri.toString();
             String previousMediaUri = sharedPreferences.getString(PREFKEY_PREVIOUS_MEDIA_URI, "");
             boolean previousMediaEnabled = sharedPreferences.getBoolean(context.getString(R.string.prefkey_previous_media_enabled), false);
             if (!previousMediaEnabled && !TextUtils.isEmpty(mediaUriText) && !TextUtils.isEmpty(previousMediaUri) && mediaUriText.equals(previousMediaUri)) {
                 // 前回と同じメディアは無視
+                showResult(PostResult.IGNORE, postType);
                 return;
             }
             sharedPreferences.edit().putString(PREFKEY_PREVIOUS_MEDIA_URI, mediaUriText).apply();
         }
 
-        PostResult result = post(postType);
-        showResult(result, postType);
-    }
-
-
-    private PostResult post(PostType postType) {
-        // 認証情報取得
-        String username = sharedPreferences.getString(context.getString(R.string.prefkey_auth_username), "");
-        String password = sharedPreferences.getString(context.getString(R.string.prefkey_auth_password), "");
-        if (TextUtils.isEmpty(username)) {
-            return PostResult.AUTH_FAILED;
-        }
-
         /** セッション */
         Session session = null;
-
         try {
             // 認証
             // フォルダ設定 (getMobileSessionの前に入れる必要あり)
@@ -259,28 +269,38 @@ public class PostIntentService extends IntentService {
             Logger.e(e);
         }
 
+        // 投稿
+        PostResult result = PostResult.IGNORE;
         try {
-            if (postType == PostType.SCROBBLE) {
-                return scrobble(session);
-            } else if (postType == PostType.LOVE) {
-                return love(session);
-            } else if (postType == PostType.UNLOVE) {
-                return unlove(session);
-            } else if (postType == PostType.BAN) {
-                return ban(session);
-            } else if (postType == PostType.UNBAN) {
-                return unban(session);
-            } else {
-                return PostResult.IGNORE;
+            switch (postType) {
+                case SCROBBLE:
+                    result = scrobble(session);
+                    break;
+                case LOVE:
+                    result = love(session);
+                    break;
+                case UNLOVE:
+                    result = unlove(session);
+                    break;
+                case BAN:
+                    result = ban(session);
+                    break;
+                case UNBAN:
+                    result = unban(session);
+                    break;
+                case PAGE_TRACK:
+                    result = trackPage(session);
+                    break;
+                case PAGE_ARTIST:
+                    result = artistPage(session);
+                    break;
             }
         } catch (Exception e) {
             Logger.e(e);
-            return PostResult.FAILED;
+            result = PostResult.FAILED;
         }
+        showResult(result, postType);
     }
-
-
-
 
     /**
      * Scrobble.
@@ -505,13 +525,72 @@ public class PostIntentService extends IntentService {
         }
     }
 
+    /**
+     * Track Page.
+     * @param session Session.
+     * @return PostResult.
+     */
+    private PostResult trackPage(Session session) {
+        if (session == null || propertyMap == null)
+            return PostResult.FAILED;
+
+        try {
+            // 無効データを無視
+            String track  = propertyMap.get(MediaProperty.TITLE.getKeyName());
+            String artist  = propertyMap.get(MediaProperty.ARTIST.getKeyName());
+            if (TextUtils.isEmpty(track) || TextUtils.isEmpty(artist))
+                return PostResult.IGNORE;
+
+            Track trackInfo = Track.getInfo(artist, track, session.getApiKey());
+            Uri uri = Uri.parse(trackInfo.getUrl());
+
+            Intent launchIntent = new Intent(Intent.ACTION_VIEW, uri);
+            launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(launchIntent);
+            return PostResult.SUCCEEDED;
+        } catch (Exception e) {
+            Logger.e(e);
+            return PostResult.FAILED;
+        }
+    }
+
+    /**
+     * Artist Page.
+     * @param session Session.
+     * @return PostResult.
+     */
+    private PostResult artistPage(Session session) {
+        if (session == null || propertyMap == null)
+            return PostResult.FAILED;
+
+        try {
+            // 無効データを無視
+            String artist  = propertyMap.get(MediaProperty.ARTIST.getKeyName());
+            if (TextUtils.isEmpty(artist))
+                return PostResult.IGNORE;
+
+            Artist artistInfo = Artist.getInfo(artist, session.getApiKey());
+            Uri uri = Uri.parse(artistInfo.getUrl());
+
+            Intent launchIntent = new Intent(Intent.ACTION_VIEW, uri);
+            launchIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(launchIntent);
+            return PostResult.SUCCEEDED;
+        } catch (Exception e) {
+            Logger.e(e);
+            return PostResult.FAILED;
+        }
+    }
+
+
+
 
     /**
      * 投稿結果を出力。
      * @param result 投稿結果。
      * @param postType 投稿種別。
      */
-    protected void showResult(PostResult result, PostType postType) {
+    private void showResult(PostResult result, PostType postType) {
         if (result == PostResult.IGNORE || result == PostResult.SAVED) {
             return;
         } else if (result == PostResult.AUTH_FAILED) {
@@ -519,45 +598,60 @@ public class PostIntentService extends IntentService {
             return;
         }
 
-        if (postType == PostType.SCROBBLE) {
-            // Scrobble
-            if (result == PostResult.SUCCEEDED) {
-                if (sharedPreferences.getBoolean(context.getString(R.string.prefkey_post_success_message_show), false)) {
-                    AppUtils.showToast(context, R.string.message_post_success); // Succeed
+        switch (postType) {
+            case SCROBBLE:
+                if (result == PostResult.SUCCEEDED) {
+                    if (sharedPreferences.getBoolean(context.getString(R.string.prefkey_post_success_message_show), false)) {
+                        AppUtils.showToast(context, R.string.message_post_success); // Succeed
+                    }
+                } else {
+                    if (sharedPreferences.getBoolean(context.getString(R.string.prefkey_post_failure_message_show), true)) {
+                        AppUtils.showToast(context, R.string.message_post_failure); // Failed
+                    }
                 }
-            } else {
-                if (sharedPreferences.getBoolean(context.getString(R.string.prefkey_post_failure_message_show), true)) {
-                    AppUtils.showToast(context, R.string.message_post_failure); // Failed
+                break;
+            case LOVE:
+                if (result == PostResult.SUCCEEDED) {
+                    AppUtils.showToast(context, context.getString(R.string.message_love_success,  propertyMap.get(MediaProperty.TITLE.getKeyName()))); // Succeed
+                } else {
+                    AppUtils.showToast(context, R.string.message_love_failure); // Failed
                 }
-            }
-        } else if (postType == PostType.LOVE) {
-            // Love
-            if (result == PostResult.SUCCEEDED) {
-                AppUtils.showToast(context, context.getString(R.string.message_love_success,  propertyMap.get(MediaProperty.TITLE.getKeyName()))); // Succeed
-            } else {
-                AppUtils.showToast(context, R.string.message_love_failure); // Failed
-            }
-        } else if (postType == PostType.UNLOVE) {
-            // UnLove
-            if (result == PostResult.SUCCEEDED) {
-                AppUtils.showToast(context, context.getString(R.string.message_unlove_success,  propertyMap.get(MediaProperty.TITLE.getKeyName()))); // Succeed
-            } else {
-                AppUtils.showToast(context, R.string.message_unlove_failure); // Failed
-            }
-        } else if (postType == PostType.BAN) {
-            // Love
-            if (result == PostResult.SUCCEEDED) {
-                AppUtils.showToast(context, context.getString(R.string.message_ban_success,  propertyMap.get(MediaProperty.TITLE.getKeyName()))); // Succeed
-            } else {
-                AppUtils.showToast(context, R.string.message_ban_failure); // Failed
-            }
-        } else if (postType == PostType.UNBAN) {
-            // UnLove
-            if (result == PostResult.SUCCEEDED) {
-                AppUtils.showToast(context, context.getString(R.string.message_unban_success,  propertyMap.get(MediaProperty.TITLE.getKeyName()))); // Succeed
-            } else {
-                AppUtils.showToast(context, R.string.message_unban_failure); // Failed
-            }
+                break;
+            case UNLOVE:
+                if (result == PostResult.SUCCEEDED) {
+                    AppUtils.showToast(context, context.getString(R.string.message_unlove_success,  propertyMap.get(MediaProperty.TITLE.getKeyName()))); // Succeed
+                } else {
+                    AppUtils.showToast(context, R.string.message_unlove_failure); // Failed
+                }
+                break;
+            case BAN:
+                if (result == PostResult.SUCCEEDED) {
+                    AppUtils.showToast(context, context.getString(R.string.message_ban_success,  propertyMap.get(MediaProperty.TITLE.getKeyName()))); // Succeed
+                } else {
+                    AppUtils.showToast(context, R.string.message_ban_failure); // Failed
+                }
+                break;
+            case UNBAN:
+                if (result == PostResult.SUCCEEDED) {
+                    AppUtils.showToast(context, context.getString(R.string.message_unban_success,  propertyMap.get(MediaProperty.TITLE.getKeyName()))); // Succeed
+                } else {
+                    AppUtils.showToast(context, R.string.message_unban_failure); // Failed
+                }
+                break;
+            case PAGE_TRACK:
+                if (result == PostResult.SUCCEEDED) {
+                    Logger.d(R.string.message_page_track_success); // Succeed
+                } else {
+                    AppUtils.showToast(context, R.string.message_page_track_failure); // Failed
+                }
+                break;
+            case PAGE_ARTIST:
+                if (result == PostResult.SUCCEEDED) {
+                    Logger.d(R.string.message_page_artist_success); // Succeed
+                } else {
+                    AppUtils.showToast(context, R.string.message_page_artist_failure); // Failed
+                }
+                break;
         }
     }
 }
