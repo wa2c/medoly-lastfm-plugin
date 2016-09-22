37c6e8e254359d730b82b95335e2af20cda5264c
entry@wa2c.com <entry@wa2c.com>
2016-09-22 18:21:44 +0900

    test

---
+++ app/src/debug/java/com/wa2c/android/medoly/plugin/action/lastfm/Token.java
@@ -0,0 +1,17 @@
+package com.wa2c.android.medoly.plugin.action.lastfm;
+
+import android.content.Context;
+
+
+
+public class Token {
+
+    public static String getKey1(Context context) {
+        return "";
+    }
+
+    public static String getKey2(Context context) {
+        return "";
+    }
+
+}
