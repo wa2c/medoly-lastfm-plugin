897e26516b738d6b666a79599ff9e5db2f31fde8
entry@wa2c.com <entry@wa2c.com>
2018-01-07 05:23:02 +0900

    - Fixed a bug
    --HG--
    branch : kotlin

--- app/src/main/java/com/wa2c/android/medoly/plugin/action/lastfm/dialog/AboutDialogFragment.kt
+++ app/src/main/java/com/wa2c/android/medoly/plugin/action/lastfm/dialog/AboutDialogFragment.kt
@@ -14,7 +14,7 @@ import android.view.View
 import android.widget.TextView
 import com.wa2c.android.medoly.plugin.action.lastfm.R
 import com.wa2c.android.medoly.plugin.action.lastfm.util.Logger
-import kotlinx.android.synthetic.main.dialog_about.*
+import kotlinx.android.synthetic.main.dialog_about.view.*
 import java.util.regex.Pattern
 
 /**
@@ -27,36 +27,36 @@ class AboutDialogFragment : AbstractDialogFragment() {
      */
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog? {
         super.onCreateDialog(savedInstanceState)
-        val layoutView = View.inflate(activity, R.layout.dialog_about, null)
+        val content = View.inflate(activity, R.layout.dialog_about, null)
 
         // Version
         try {
             val packageInfo = activity.packageManager.getPackageInfo(activity.packageName, PackageManager.GET_ACTIVITIES)
-            (layoutView.findViewById(R.id.dialogAboutAppVersionTextView) as TextView).text = getString(R.string.label_dialog_about_ver, packageInfo.versionName)
+            content.dialogAboutAppVersionTextView.text = getString(R.string.label_dialog_about_ver, packageInfo.versionName)
         } catch (e: PackageManager.NameNotFoundException) {
             Logger.e(e)
         }
 
         // Email
-        dialogAboutEmailTextView.text = getString(R.string.app_mail_name) + "@" + getString(R.string.app_mail_domain)
+        content.dialogAboutEmailTextView.text = getString(R.string.app_mail_name) + "@" + getString(R.string.app_mail_domain)
 
         // License
         Linkify.addLinks(
-                dialogAboutLicenseTextView,
+                content.dialogAboutLicenseTextView,
                 Pattern.compile(getString(R.string.app_license)),
                 getString(R.string.app_license_url), null,
                 Linkify.TransformFilter { _, _ -> getString(R.string.app_license_url) })
 
         // Privacy Policy
         Linkify.addLinks(
-                dialogAboutPrivacyPolicyTextView,
+                content.dialogAboutPrivacyPolicyTextView,
                 Pattern.compile(getString(R.string.label_dialog_about_link)),
                 getString(R.string.app_privacy_policy_url), null,
                 Linkify.TransformFilter { _, _ -> getString(R.string.app_privacy_policy_url) })
 
         // Google Play
         Linkify.addLinks(
-                dialogAboutGooglePlayTextView,
+                content.dialogAboutGooglePlayTextView,
                 Pattern.compile(getString(R.string.label_dialog_about_link)),
                 getString(R.string.app_market_web), null,
                 Linkify.TransformFilter { _, _ -> getString(R.string.app_market_web) })
@@ -75,14 +75,14 @@ class AboutDialogFragment : AbstractDialogFragment() {
             }
             libTextView.gravity = Gravity.CENTER_HORIZONTAL
             libTextView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20f)
-            dialogAboutLibraryLayout.setPadding(2, 2, 2, 2)
-            dialogAboutLibraryLayout.addView(libTextView)
+            content.dialogAboutLibraryLayout.setPadding(2, 2, 2, 2)
+            content.dialogAboutLibraryLayout.addView(libTextView)
         }
 
         // ダイアログ作成
         val builder = AlertDialog.Builder(activity)
         builder.setTitle(R.string.pref_title_about)
-        builder.setView(layoutView)
+        builder.setView(content)
         builder.setNeutralButton(android.R.string.ok, null)
         return builder.create()
     }
